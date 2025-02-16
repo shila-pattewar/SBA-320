008cbeba05ebbd34b3d081137f49ebd36a0e8cec
shila <shilapattewar16@gmail.com>
2025-02-15 23:39:19 -0600

    added fetch api syntax from websit to call api

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -3,7 +3,19 @@ import { useEffect } from "react";
 function Product() {
   useEffect(() => {
     console.log("loading use effect");
-  });
+
+    // calling fakeApi
+
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="products">
